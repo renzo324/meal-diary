438dd06b999eebacf91c1f4006a3c1a7d77c5135
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-11-16 05:22:46 +0800

    full rebuild login functionality

--- src/Store/Actions/entryActions.js
+++
@@ -1,6 +0,0 @@
-export const addMeal = (entry) => {
-    return (dispatch, getState) => {
-        //Async call to DB
-        dispatch({ type: 'ADD_ENTRY', entry});
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import { createStore, applyMiddleware } from 'redux';
-import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './Store/Reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import App from './components/App';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
