7c6bfb214c02cc5fc70f85bea5d269a666bbcde0
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-11-21 00:48:21 +0800

    proper sql package

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
-import mysql from 'mysql';
-
+// const mysql = require("mysql");
+var mysql      = require('mysql');
+// connection
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    passworg: "",
-    database: ""
-})
-
-conn.connect(function(err){
-    if (err) throw err;
-    console.log("Connected To The Database");
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: ""
 });
+
+// Connection
+conn.connect(function(err) {
+  // check for errors
+  if (err) throw err;
+  // else do database things
+  console.log("Connection complete");
+
+  // create database
+  // eslint-disable-next-line no-shadow
+//   conn.query("CREATE DATABASE nodeSQL", function(err, result) {
+//     if (err) throw err;
+//     console.log(`database created!${result}`);
+  });
+
